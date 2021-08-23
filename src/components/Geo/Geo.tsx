0eb7da836af3906de39bf0c93cdc98446dfb4382
Victor Lin <13424970+victorlin@users.noreply.github.com>
2021-08-22 19:51:24 -0700

    leaflet example working

--- src/components/Geo/Geo.tsx
+++ src/components/Geo/Geo.tsx
@@ -4,53 +4,24 @@ import { MapPlot } from './MapPlot'
 import { SelectionInfo } from './SelectionInfo'
 import { RunData } from './types'
 import { helpIcon } from 'common'
+// @ts-ignore
+// import PixiOverlay from 'react-leaflet-pixi-overlay'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import { LatLngExpression } from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 
 export const Geo = () => {
-    const [selectedPoints, setSelectedPoints] = React.useState<RunData[]>()
-    const [isCollapsed, setIsCollapsed] = React.useState<boolean>(false)
-
-    const headTags = (
-        <Helmet>
-            <title>Serratus | Planetary RNA Virome</title>
-        </Helmet>
-    )
+    const defaultPosition: LatLngExpression = [48.864716, 2.349]
 
     return (
-        <div className='mx-4 my-2'>
-            {headTags}
-            <div className='text-center text-xl'>The Planetary RNA Virome</div>
-
-            <button
-                className='text-left collapse-button'
-                onClick={() => setIsCollapsed(!isCollapsed)}>
-                {helpIcon} Info
-            </button>
-            <div
-                className={`collapse-content ${!isCollapsed ? 'collapsed' : 'expanded'}`}
-                aria-expanded={isCollapsed}>
-                <p>
-                    We searched 5.7 million public sequencing libraries for the RNA virus hallmark
-                    gene, RNA-dependent RNA Polymerase (RdRP).
-                </p>
-
-                <p>
-                    This map shows the location of BioSamples from which an intact RdRP sequence
-                    could be recovered and geographical meta-data was present.
-                </p>
-
-                <p>A 100-meter randomization is applied to all points to prevent overplotting.</p>
-            </div>
-
-            <div className='my-2'>
-                <MapPlot setSelectedPoints={setSelectedPoints} />
-            </div>
-
-            <div className='text-left text-gray-600'>
-                Use <b>`Shift`</b>-click to select multiple points or the <b>`Box Select`</b> or{' '}
-                <b>`Lasso Select`</b> icons in the top-right.
-            </div>
-
-            <SelectionInfo selectedPoints={selectedPoints} />
-        </div>
+        <MapContainer
+            center={defaultPosition}
+            zoom={13}
+            style={{ height: '600px', width: '600px' }}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            />
+        </MapContainer>
     )
 }
