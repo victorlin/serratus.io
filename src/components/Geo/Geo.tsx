305f3c5ad354fafbaecc5958dea6c39891454fcd
Victor Lin <13424970+victorlin@users.noreply.github.com>
2021-08-22 20:40:07 -0700

    react-leaflet-pixi-overlay example working

--- src/components/Geo/Geo.tsx
+++ src/components/Geo/Geo.tsx
@@ -5,23 +5,41 @@ import { SelectionInfo } from './SelectionInfo'
 import { RunData } from './types'
 import { helpIcon } from 'common'
 // @ts-ignore
-// import PixiOverlay from 'react-leaflet-pixi-overlay'
-import { MapContainer, TileLayer } from 'react-leaflet'
-import { LatLngExpression } from 'leaflet'
+import PixiOverlay from 'react-leaflet-pixi-overlay'
+// @ts-ignore
+import { Map, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 export const Geo = () => {
-    const defaultPosition: LatLngExpression = [48.864716, 2.349]
+    const markers = [
+        {
+            id: 'randomStringOrNumber',
+            iconColor: 'red',
+            position: [-37.814, 144.96332],
+            // popup: renderToString(
+            //   <div>All good!</div>
+            // ),
+            onClick: () => alert('marker clicked'),
+            tooltip: 'Hey!',
+        },
+        {
+            id: '2',
+            iconColor: 'blue',
+            position: [-37.814, 144.96332],
+            popup: 'Quack!',
+            popupOpen: true, // if popup has to be open by default
+            onClick: () => alert('marker clicked'),
+            tooltip: 'Nice!',
+        },
+    ]
 
     return (
-        <MapContainer
-            center={defaultPosition}
-            zoom={13}
-            style={{ height: '600px', width: '600px' }}>
+        <Map center={[-37.814, 144.96332]} zoom={13} style={{ height: '600px', width: '600px' }}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
             />
-        </MapContainer>
+            <PixiOverlay markers={markers} />
+        </Map>
     )
 }
